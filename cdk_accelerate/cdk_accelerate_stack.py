38f852caf340d67eeef1bb989b483a7a7067e05d
user <user@users-iMac.local>
2023-01-09 20:07:57 +0100

    lambda resolvers added

--- cdk_accelerate/cdk_accelerate_stack.py
+++ cdk_accelerate/cdk_accelerate_stack.py
@@ -1,3 +1,5 @@
+from os import path
+import os
 from constructs import Construct
 from aws_cdk import (
     Duration,
@@ -7,6 +9,9 @@ from aws_cdk import (
     aws_sns as sns,
     aws_sns_subscriptions as subs,
 )
+import aws_cdk.aws_appsync as appsync
+from aws_cdk import aws_lambda as lambda_
+
 
 
 class CdkAccelerateStack(Stack):
@@ -19,8 +24,74 @@ class CdkAccelerateStack(Stack):
             visibility_timeout=Duration.seconds(300),
         )
 
-        topic = sns.Topic(
-            self, "CdkAccelerateTopic"
+        api = appsync.CfnGraphQLApi(self, "Api", 
+        name="demo",
+        authentication_type="API_KEY"
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        data_schema = ''
+        with open("../schema.graphql", 'r') as file:
+            data_schema = file.read().replace('\n', '')
+
+        schema = appsync.CfnGraphQLSchema(scope=self, id="schema", api_id=api.attr_api_id, definition=data_schema)
+
+        lambdaDs_function = lambda_.CfnFunction(self, "MyCfnFunction",
+            code=lambda_.CfnFunction.CodeProperty(
+                image_uri="imageUri",
+                s3_bucket="s3Bucket",
+                s3_key="s3Key",
+                s3_object_version="s3ObjectVersion",
+                zip_file="zipFile"
+            ),
+            role="role",
+
+            # the properties below are optional
+            architectures=["x86_64"],
+            description="lambda-ds",
+            environment=lambda_.CfnFunction.EnvironmentProperty(
+                variables={
+                    "TABLE": "variables"
+                }
+            ),
+            function_name="order-lambda-ds",
+            handler="handler",
+            package_type="zip",
+            runtime="python3.9",
+            timeout=123,
+            tracing_config=lambda_.CfnFunction.TracingConfigProperty(
+                mode="Active"
+            )
+        )
+
+        lambda_config_property = appsync.CfnDataSource.LambdaConfigProperty(
+            lambda_function_arn=lambdaDs_function.attr_arn
+        )
+        
+        lambdaDs = appsync.CfnDataSource(self, "lambda-ds", api.attr_api_id, "lambda-ds", type="AWS_LAMBDA",
+        lambda_config=lambda_config_property)
+
+        list_orders = appsync.CfnResolver(self, "list-orders",
+        api_id=api.attr_api_id,
+        field_name="orders",
+        type_name="query")
+
+        get_order = appsync.CfnResolver(self, "get-order",
+        api_id=api.attr_api_id,
+        field_name="order",
+        type_name="query")
+
+        post_order = appsync.CfnResolver(self, "post-order",
+        api_id=api.attr_api_id,
+        field_name="postOrder",
+        type_name="mutation")
+
+        update_order = appsync.CfnResolver(self, "update-order",
+        api_id=api.attr_api_id,
+        field_name="updateOrder",
+        type_name="mutation")
+
+        delete_order = appsync.CfnResolver(self, "delete-order",
+        api_id=api.attr_api_id,
+        field_name="deleteOrder",
+        type_name="mutation")
+
